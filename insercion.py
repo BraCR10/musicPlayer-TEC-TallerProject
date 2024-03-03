b0b527a5dcb0a94760f64c8314fe173e02bf0378
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-03-02 23:44:38 -0600

    Cambios a insercion

--- insercion.py
+++ insercion.py
@@ -18,10 +18,10 @@ def insertProp():
                     nuevo[1] = nuevo[1].replace('\n', '') #Elimina cada '\n' en cada elemento [1]
                     propietarios+=[nuevo]
                 elif len(nuevo)==3:#Validacion 
-                    album+=[nuevo[:2]]
+                    propietarios+=[nuevo[:2]]
                 else:#Validacion 
                     continue          
-    return cod
+    return cod,propietarios
 
 #Esta funcion lee un fichero llamado Genero.txt y retorna cada linea en una lista dentro de otra lista
 def insertGen():
@@ -42,16 +42,18 @@ def insertGen():
                 if len(nuevo)==2:#Validacion 
                     nuevo[1] = nuevo[1].replace('\n', '') #Elimina cada '\n' en cada elemento [1]
                     Generos+=[nuevo]
+                elif len(nuevo)==3:#Validacion 
+                    Generos+=[nuevo[:2]]
                 else:#Validacion 
                     continue          
-    return cod
+    return cod,Generos
 
 #Esta funcion lee un fichero llamado Artista.txt y retorna cada linea en una lista dentro de otra lista
 def insertArt():
     texto = open('Artista.txt', 'r')
     artistasOri=texto.readlines()#Se crea una lista, cada linea en el fichero es un elemento
     texto.close()
-    artista=[]#Lista para almacenar cambios en los datos del fichero
+    artistas=[]#Lista para almacenar cambios en los datos del fichero
     cod=[]#Almacena codigos
     codValidos=[]
     for linea in artistasOri:#Itera en lista sin cambios
@@ -66,23 +68,25 @@ def insertArt():
                 if len(nuevo)==3:#Validacion 
                     nuevo[2] = nuevo[2].replace('\n', '') #Elimina cada '\n' en cada elemento [2]
                     #Obtiene la lista de codigos registrados
-                    if nuevo[2] in insertGen():#Agrega solo si el genero esta
-                        artista+=[nuevo] 
+                    id,todo=insertGen()
+                    if nuevo[2] in id:#Agrega solo si el genero esta
+                        artistas+=[nuevo] 
                         codValidos+=[nuevo[0]]
                 elif len(nuevo)==4:#Validacion 
-                    if nuevo[2] in insertGen():#Agrega solo si el genero esta
-                        artista+=[nuevo[:3]]
+                    id,todo=insertGen()
+                    if nuevo[2] in id:#Agrega solo si el genero esta
+                        artistas+=[nuevo[:3]]
                         codValidos+=[nuevo[0]]
                 else:#Validacion 
                     continue     
-    return codValidos
+    return codValidos,artistas
 
 #Esta funcion lee un fichero llamado Playlist.txt y retorna cada linea en una lista dentro de otra lista
 def insertPlaylist():
     texto = open('Playlist.txt', 'r')
     playlistOri=texto.readlines()#Se crea una lista, cada linea en el fichero es un elemento
     texto.close()
-    playlist=[]#Lista para almacenar cambios en los datos del fichero
+    playlists=[]#Lista para almacenar cambios en los datos del fichero
     cod=[]#Almacena codigos
     codValidos=[]
     for linea in playlistOri:#Itera en lista sin cambios
@@ -96,25 +100,25 @@ def insertPlaylist():
                 cod+=[nuevo[0]]
                 if len(nuevo)==3:#Validacion 
                     nuevo[2] = nuevo[2].replace('\n', '') #Elimina cada '\n' en cada elemento [2]
-                    codprop=insertProp()#Obtiene la lista de codigos registrados
+                    codprop,todo=insertProp()#Obtiene la lista de codigos registrados
                     if nuevo[2] in codprop:#Agrega solo si el propietario esta
-                        playlist+=[nuevo] 
+                        playlists+=[nuevo] 
                         codValidos+=[nuevo[0]]
                 elif len(nuevo)==4:#Validacion 
-                    codprop=insertProp()
+                    codprop,todo=insertProp()#Obtiene la lista de codigos registrados
                     if nuevo[2] in codprop:#Agrega solo si el genero esta
-                        playlist+=[nuevo[:3]]
+                        playlists+=[nuevo[:3]]
                         codValidos+=[nuevo[0]]
                 else:#Validacion 
                     continue     
-    return codValidos
+    return codValidos,playlists
 
 #Esta funcion lee un fichero llamado Album.txt y retorna cada linea en una lista dentro de otra lista
 def insertAlbum(): 
     texto = open('Album.txt', 'r')
     albumOri=texto.readlines()#Se crea una lista, cada linea en el fichero es un elemento
     texto.close()
-    album=[]#Lista para almacenar cambios en los datos del fichero
+    albums=[]#Lista para almacenar cambios en los datos del fichero
     cod=[]#Almacena codigos para que no se repitan
     codValidos=[]#Almacena codigos que cumple todas las condiciones
     for linea in albumOri:#Itera en lista sin cambios
@@ -128,18 +132,18 @@ def insertAlbum():
                 cod+=[nuevo[0]]
                 if len(nuevo)==3:#Validacion 
                     nuevo[2] = nuevo[2].replace('\n', '') #Elimina cada '\n' en cada elemento [2]
-                    codart=insertArt()#Obtiene la lista de codigos registrados
+                    codart,todo=insertArt()#Obtiene la lista de codigos registrados
                     if nuevo[2] in codart:#Agrega solo si el propietario esta
-                        album+=[nuevo] 
+                        albums+=[nuevo] 
                         codValidos+=[nuevo[0]]
                 elif len(nuevo)==4:#Validacion 
-                    codart=insertArt()
+                    codart,todo=insertArt()#Obtiene la lista de codigos registrados
                     if nuevo[2] in codart:#Agrega solo si el propietario esta
-                        album+=[nuevo[:3]]
+                        albums+=[nuevo[:3]]
                         codValidos+=[nuevo[0]]
                 else:#Validacion 
                     continue     
-    return codValidos
+    return codValidos,albums
 
 #Esta funcion lee un fichero llamado Canciones.txt y retorna cada linea en una lista dentro de otra lista
 def insertCanciones(): 
@@ -160,22 +164,22 @@ def insertCanciones():
                 cod+=[nuevo[0]]
                 if len(nuevo)==6:#Validacion 
                     nuevo[5] = nuevo[5].replace('\n', '') #Elimina cada '\n' en cada elemento [2]
-                    codArt=insertArt()#Obtiene la lista de codigos registrados
-                    codPlaylist=insertPlaylist()
-                    codGen=insertGen()
-                    codAlbum=insertAlbum()
+                    codArt,todo=insertArt()#Obtiene la lista de codigos registrados
+                    codPlaylist,todo=insertPlaylist()
+                    codGen,todo=insertGen()
+                    codAlbum,todo=insertAlbum()
                     if nuevo[2] in codArt and nuevo[3] in codAlbum and nuevo[4] in codGen and nuevo[5] in codPlaylist :#Agrega solo si los otros estan
                         canciones+=[nuevo] 
                         codValidos+=[nuevo[0]]
                 elif len(nuevo)==7:#Validacion 
-                    codArt=insertArt()#Obtiene la lista de codigos registrados
-                    codPlaylist=insertPlaylist()
-                    codGen=insertGen()
-                    codAlbum=insertAlbum()
+                    codArt,todo=insertArt()#Obtiene la lista de codigos registrados
+                    codPlaylist,todo=insertPlaylist()
+                    codGen,todo=insertGen()
+                    codAlbum,todo=insertAlbum()
                     if (nuevo[2] in codArt) and (nuevo[3] in codAlbum) and (nuevo[4] in codGen) and (nuevo[5] in codPlaylist) :#Agrega solo si los otros estan
                         canciones+=[nuevo[:6]] 
                         codValidos+=[nuevo[0]]
                 else:#Validacion 
                     continue     
-    return codValidos
+    return codValidos,canciones
 
