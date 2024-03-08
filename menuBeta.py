d8674f6d9709966ae68192710f32c93633f60e0c
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-03-07 19:26:39 -0600

    Implementacion de def volver y no existe

--- menuBeta.py
+++ menuBeta.py
@@ -6,7 +6,23 @@
 from insercion import *#insertAlbum,insertArt,insertCanciones,insertGen,insertPlaylist,insertProp
 from busqueda import * #buscarAlbum,buscarArtista,buscarCancion,buscarGenero,buscarPlaylist,buscarProp
 from eliminacion import *#eliminarProp,eliminarCanciones,eliminarPlaylist
+def noExiste():
+    print('\n ---> Esta opcion no exite')
+    print('\nVolver al menu principal o salir?:')
+    print('\n1- Volver al menu principal')   
+    print('2- Salir de reproductor')  
+    opcion=int(input('\nEscoja un numero segun la accion que desea realizar:  '))
+    if opcion == 1:
+        return 1
 
+    
+def volver():
+    print('\nVolver al menu principal o salir?:')
+    print('\n1- Volver al menu principal')   
+    print('2- Salir de reproductor')  
+    opcion=int(input('\nEscoja un numero segun la accion que desea realizar: '))
+    if opcion==1:
+        return 1 
 def menu():
     listaPropcod,listaProptodo=insertProp()
     listaGencod,listaGentodo=insertGen()
@@ -34,21 +50,12 @@ def menu():
                     print('\n ---> El propietario no existe')
                 else:
                     print('\n ---> El propietario es: ',buscarProp(dato,listaProptodo))
-                print('\nVolver al menu principal o salir?:')
-                print('\n1- Volver al menu principal')   
-                print('2- Salir de reproductor')  
-                opcion=int(input('\nEscoja un numero segun la accion que desea realizar: '))
-                if opcion==1:
+                if volver()==1:
                     continue
                 else:
                     break
             else:
-                print('\n ---> Esta opcion no exite')
-                print('\nVolver al menu principal o salir?:')
-                print('\n1- Volver al menu principal')   
-                print('2- Salir de reproductor')  
-                opcion=int(input('\nEscoja un numero segun la accion que desea realizar:  '))
-                if opcion == 1:
+                if noExiste() == 1:
                     continue
                 else:
                     break
@@ -57,15 +64,18 @@ def menu():
         elif opcion==3:#Eliminaciones
             print('\nLista de opciones:\n')
             print('1- Eliminar Propietario') 
+            print('2- Eliminar Playlist') 
+            print('3- Eliminar Genero') 
+            print('4- Eliminar Artista') 
+            print('5- Eliminar Album') 
             print('6- Eliminar Cancion') 
-            opcion=int(input('\nDigite un numero: '))
+            opcion=int(input('\nEscoja un numero segun la accion que desea realizar: '))
             if opcion==1:
                 dato=str(input('Digite el codigo a eliminar: '))
-                listaPropcod,listaProptodo=eliminarProp(dato)
+                listaPropcod,listaProptodo=eliminarProp(dato)#Actualiza la lista si el codigo no existe, la deja igual
                 for i in listaPlaylisttodo:
-                    if i[2]==dato:
-                        listaPlaylistcod,listaPlaylisttodo=eliminarPlaylist(i[0])
-            
+                    if i[2]==dato:#Los elemento [2] de todo lista contienen codProp
+                        listaPlaylistcod,listaPlaylisttodo=eliminarPlaylist(i[0])#Actualiza la lista si el codigo no existe, la deja igual
                 print('\n Ha sido eliminado correctamente')
             if opcion==6:
                 dato=str(input('Digite el codigo a eliminar: '))
